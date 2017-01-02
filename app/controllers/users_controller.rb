1645d31794993d0ab617487877045fc0318289e4
limdan7 <limdan7@berkeley.edu>
2017-01-02 03:01:20 +0000

    Edit Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,20 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+    
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.save
+      flash[:success] = "Successfully Updated. #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'update'
+    end
+  end
   private
     def user_params
       params.require(:user).permit(:username,:email,:password)
